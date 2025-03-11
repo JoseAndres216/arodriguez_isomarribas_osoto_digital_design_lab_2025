cfb448b0182098df68eb24c0fe5764f04b624674
Óscar Soto Varela <62962211+CAMANEM@users.noreply.github.com>
2025-03-11 16:19:08 -0600

    initial commit

---
+++ Problem3.1/digital_design_lab1_problem3.sv
@@ -0,0 +1,7 @@
+module digital_design_lab1_problem3
+	#(parameter n = 6)
+	(input logic reset, dcrease_btn, output logic isNegative, output logic [n-1:0] value, output logic [6:0] segUnits, segDec, segSign);
+	
+	
+	
+	
