3a4dd7a6416c56243c731849dc14aec9a93e90f8
user <camanem10@hotmail.com>
2025-03-12 18:57:20 -0600

    Revert "Increaser"
    This reverts commit 777662c8c095dcd9660f6ee5d2a0e460a73f194f.

--- Problem3.1/digital_design_lab1_problem3.sv
+++ Problem3.1/digital_design_lab1_problem3.sv
@@ -1,6 +1,6 @@
 module digital_design_lab1_problem3
 	#(parameter n = 6)
-	(input logic reset, decrease_btn, increase_btn, output logic [6:0] segUnits, segTens, segSign, output logic [n-1:0] value, output logic isNegative);
+	(input logic reset, decrease_btn, output logic [6:0] segUnits, segTens, segSign, output logic [n-1:0] value, output logic isNegative);
 	
 	logic [3:0] valueUnits;  
 	logic [3:0] valueTens;  
@@ -12,7 +12,7 @@ module digital_design_lab1_problem3
 
 	end
 	
-	always @(posedge decrease_btn or posedge reset or posedge increase_btn) 
+	always @(posedge decrease_btn or posedge reset) 
 		begin
 			if (reset) 
 				begin 
@@ -24,40 +24,20 @@ module digital_design_lab1_problem3
 				begin
 					if (value === 0)
 						begin
-							isNegative <= 1;
-							segSign <= 7'b0111111; // -
-							value <= value + 1;
+							isNegative = 1;
+							segSign = 7'b0111111; // -
+							value <= value + 1;  // Decrementar en 1
 						end
-					else if ((value == (2**n - 1)) && (isNegative === 1)) // max number
+					else if (value == (2**n - 1)) // max number
 						begin
 							// reached minimum number
 						end
 					else if (isNegative === 1)
-							value <= value + 1; 
+							value <= value + 1;  // Decrementar en 1
 					else if (isNegative === 0)
-						value <= value - 1;  
+						value <= value - 1;  // Decrementar en 1
 				end
-			else if (increase_btn) // increase protocol
-				begin
-					if ((value === 1) && (isNegative == 1))
-						begin
-							isNegative <= 0;
-							segSign <= 7'b1111111; // -
-							value <= value - 1; 
-						end
-					else if ((value == (2**n - 1)) && (isNegative === 0)) // max number
-						begin
-							// reached minimum number
-						end
-					else if (isNegative === 1)
-						begin
-							value <= value - 1;
-						end
-					else if (isNegative === 0)
-					begin
-						value <= value + 1;
-					end
-				end
+			//else if con sumador
 				
 			// conversion to only units and tens from value
 			 valueUnits = value % 10;  
