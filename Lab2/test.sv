9b987e5709a58ac7823585a8e29bc70afcd366f0
Óscar Soto Varela <62962211+CAMANEM@users.noreply.github.com>
2025-03-28 16:46:48 -0600

    Problem 2 Code: Test

--- Lab2/test.sv
+++ Lab2/test.sv
@@ -1,40 +1,38 @@
 // Problema 2: Frencuencia Máxima
-module test #(parameter N=16)(
-    input logic [N-1:0] a, b,
-    input logic selbutton,
+module test #(parameter Nbit=32)(
     input logic clk,              // Clock signal
-    input logic reset, 
-    // Señales de salida
-    output logic [N-1:0] result,
-    output logic [3:0] flags_salida, select
+    input logic reset,            // Reset signal
+    input logic [Nbit-1:0] A, B,  // Operand A and B
+    input logic [3:0] OperationIn,// Operation code // Agregar este Select a un registro
+    output logic [Nbit-1:0] out,  // Output data
+    output logic N, Z, C, V       // Flags
 );
 
     // Internal signals
-    logic [N-1:0] reg_A, reg_B, reg_out, alu_result;
-
+    logic [Nbit-1:0] reg_A, reg_B, reg_out, alu_result, reg_operation;
+  // Instantiate ALU
+    alu #(.N(Nbit)) ALU_inst (
+        .a(reg_A),               // Use reg_A as operand A
+        .b(reg_B),               // Use reg_B as operand B
+        .selbutton(reg_operation),
+        .result(alu_result)                
+    );
     // Registers for inputs A and B
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            reg_A <= {N{1'b0}};
-            reg_B <= {N{1'b0}};
-            reg_out <= {N{1'b0}};
+            reg_A <= {Nbit{1'b1}};
+            reg_B <= {Nbit{1'b1}};
+            reg_out <= {Nbit{1'b0}};
+				reg_operation <= {Nbit{4'b0000}};
         end else begin
-            reg_A <= a;        // Load input A to reg_A
-            reg_B <= b;        // Load input B to reg_B
+            reg_A <= A;        // Load input A to reg_A
+            reg_B <= B;        // Load input B to reg_B
             reg_out <= alu_result; // Load ALU result to reg_out
+				reg_operation <= OperationIn;
         end
     end
 
-    // Instantiate ALU
-    alu #(.N(N)) uut (
-        .a(a),
-        .b(b),
-        .selbutton(selbutton),
-        .result(result),
-        .flags_salida(flags_salida),
-        .select(select),
-        .seg_a_U(), .seg_a_T(), .seg_b_U(), .seg_b_T(), .seg_r_U(), .seg_r_T()
-    );
+  
 
     // Output result from the final register
     assign out = reg_out;
