c8f126a5ff6b6c1b943810a8f34ff4505947d1e2
Óscar Soto Varela <62962211+CAMANEM@users.noreply.github.com>
2025-06-15 15:45:27 -0600

    Functional Book Example
    Co-Authored-By: Isaac Somarribas <103971189+BloodXe@users.noreply.github.com>

--- FinalProject/imem.sv
+++ FinalProject/imem.sv
@@ -3,7 +3,7 @@ module imem(input logic [31:0] a,
 				
 	logic [31:0] RAM[1023:0];
 	initial
-	$readmemh("C:/Users/bloodxe/Documents/arodriguez_isomarribas_osoto_digital_design_lab_2025/FinalProject/memfile.dat", RAM);
+	$readmemh("C:/Users/caman/Documents/Github/arodriguez_isomarribas_osoto_digital_design_lab_2025/FinalProject/memfile.dat", RAM);
 	
 	assign rd = RAM[a[31:2]]; // word aligned
 	
